e134543ef2a9b83b383100f80efccc86d782696f
cocaine666 <63399143+cocaine666@users.noreply.github.com>
2021-03-19 15:13:06 +0100

    small update
    - Added possibility of downloading videos without reopening the program
    - Added Exit option (some people don't type in the right link and they just want to retry)

--- ytdl.py
+++ ytdl.py
@@ -12,17 +12,25 @@ def input_link():
     return address
 
 if __name__ == "__main__":
-    print("YouTube Downloader by malbasic - 1.0.0.0")
-    print(" ")
-    yt_obj = YouTube(input_link())
-    print("Loading video information...")
-    print(" ")
-    print("Video Title: ", yt_obj.title)
-    print(" ")
-    confirm = input("Do you want to confirm the download? Y/N ")
-    if confirm == 'Y':
-        yt_stream = yt_obj.streams.filter(only_audio=True).first()
-        yt_stream.download(resource_path("C:/"))
-    if confirm == 'N':
-        exit()
-
+    clear_console = lambda: os.system('cls')
+    run_dialog = True
+    while run_dialog:
+        clear_console()
+        print("YouTube Music Downloader by malbasic - 1.0.0.0")
+        print(" ") # Spacer...  lel
+        print("Current download location --> C:// ")
+        print(" ")
+        yt_obj = YouTube(input_link())
+        print("Loading video information...")
+        print(" ")
+        print("Video Title: ", yt_obj.title)
+        print(" ")
+        confirm = input("Do you want to confirm the download? Y/N/E ")
+        if confirm == 'Y':
+            yt_stream = yt_obj.streams.filter(only_audio=True).first()
+            yt_stream.download(resource_path("C:/"))
+        if confirm == 'N':
+            clear_console()
+        if confirm == 'E':
+            run_dialog = False
+    exit()
