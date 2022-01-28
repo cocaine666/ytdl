e0288212ea703195945405bc1ba912680baf9161
malbasic <63399143+kmalbasic@users.noreply.github.com>
2022-01-28 20:53:43 +0100

    updated ytdl - GUI implemented

--- ytdl.py
+++ ytdl.py
@@ -1,36 +1,41 @@
-import pytube
 from pytube import YouTube
 import os
 import sys
+import tkinter
+from tkinter import *
+import tk
+
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
 
-def input_link():
-    address = str(input("Please input your YouTube video link: "))
-    return address
-
-if __name__ == "__main__":
-    clear_console = lambda: os.system('cls')
-    run_dialog = True
-    while run_dialog:
-        clear_console()
-        print("YouTube Music Downloader by malbasic - 1.0.0.0")
-        print(" ") # Spacer...  lel
-        print("Current download location --> C:// ")
-        print(" ")
-        yt_obj = YouTube(input_link())
-        print("Loading video information...")
-        print(" ")
-        print("Video Title: ", yt_obj.title)
-        print(" ")
-        confirm = input("Do you want to confirm the download? Y/N/E ")
-        if confirm == 'Y':
+def download(audio):
+    if video_input.get() != "":
+        yt_obj = YouTube(video_input.get())
+        if audio:
             yt_stream = yt_obj.streams.filter(only_audio=True).first()
             yt_stream.download(resource_path("C:/"))
-        if confirm == 'N':
-            clear_console()
-        if confirm == 'E':
-            run_dialog = False
-    exit()
+        else:
+            yt_stream = yt_obj.streams.filter(only_audio=False).first()
+            yt_stream.download(resource_path("C:/"))
+    else:
+        return
+
+obj = tkinter.Tk()
+obj.title('YTDL')
+obj.geometry("250x125")
+obj.iconbitmap(r'yt.ico')
+
+padding_label = tkinter.Label(obj, text="        ").grid(row=0, column=2)
+link_label = tkinter.Label(obj, text='Link:').grid(row=2,column=3)
+video_input = tkinter.Entry(width=25)
+cb = BooleanVar()
+run_bool = False
+conversion_chkbox = tkinter.Checkbutton(obj, variable=cb, text="Convert to MP3").grid(row=3,column=3)
+dl_button = tkinter.Button(obj, text='Download', width=25, command=lambda : download(cb.get())).grid(row=4,column=3)
+
+
+video_input.grid(row=2,column=3)
+
+obj.mainloop()
